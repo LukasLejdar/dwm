b2ee699a08d2a238f6f34d6905278a3a5b821d13
Lukáš Lejdar <lejdosl@gmail.com>
2023-08-28 13:10:00 +0200

    patch alpha

--- config.def.h
+++ config.def.h
@@ -17,10 +17,17 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = OPAQUE;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+    /*               fg      bg        border*/
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -33,7 +40,7 @@ static const Rule rules[] = {
 	 */
 	/* class            instance    title       tags mask     isfloating   monitor */
 	{ "Google-chrome",  NULL,       NULL,       2,            0,           -1 },
-	{ "Code",  NULL,       NULL,       3,            0,           -1 },
+	{ "Code",           NULL,       NULL,       4,            0,           -1 },
 };
 
 /* layout(s) */
